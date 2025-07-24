7019d5331a5b6ade8a96b1c237bfc7bfd0d7e4d3
typ49 <typ490@gmail.com>
2025-07-24 17:35:31 +0200

    accordion section

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ function onClick(element) {
 }
 
 // Change style of navbar on scroll
-window.onscroll = function () { myFunction() };
-function myFunction() {
+window.onscroll = function () { navbarFunction() };
+function navbarFunction() {
     var navbar = document.getElementById("myNavbar");
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-theme-l3";
@@ -18,11 +18,20 @@ function myFunction() {
 }
 
 // Used to toggle the menu on small screens when clicking on the menu button
-function toggleFunction() {
+function toggleNavbarFunction() {
     var x = document.getElementById("navDemo");
     if (x.className.indexOf("w3-show") == -1) {
         x.className += " w3-show";
     } else {
         x.className = x.className.replace(" w3-show", "");
     }
+}
+
+function accordionFunction(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
 }

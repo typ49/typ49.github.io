18058b565e5014d312ef8ea29734a2ba443acbb3
typ49 <typ490@gmail.com>
2025-08-05 21:09:47 +0200

    typo + update SVG fill colors for dark mode + update mobile display

--- js/darkmod.js
+++ js/darkmod.js
@@ -6,24 +6,34 @@ const lightLogo = "assets/LOGO-light.png";
 document.addEventListener('DOMContentLoaded', function () {
     const link = document.getElementById("theme");
     const logo = document.getElementById("logo");
+    const svg1 = document.getElementById("svg1");
+    const svg2 = document.getElementById("svg2");
 
     const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 
     if (isDarkMode) {
         link.setAttribute("href", cssDarkTheme);
         logo.setAttribute("src", darkLogo)
+        svg1.setAttribute("fill", "#1B2D62")
+        svg2.setAttribute("fill", "#1B2D62")
     } else {
         link.setAttribute("href", cssLightTheme);
         logo.setAttribute("src", lightLogo);
+        svg1.setAttribute("fill", "#3659c4")
+        svg2.setAttribute("fill", "#3659c4")
     }
 
     window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', ({ matches }) => {
         if (matches) {
             link.setAttribute("href", cssDarkTheme);
             logo.setAttribute("src", darkLogo);
+            svg1.setAttribute("fill", "#1B2D62")
+            svg2.setAttribute("fill", "#1B2D62")
         } else {
             link.setAttribute("href", cssLightTheme);
             logo.setAttribute("src", lightLogo);
+            svg1.setAttribute("fill", "#3659c4")
+            svg2.setAttribute("fill", "#3659c4")
         }
     });
 
